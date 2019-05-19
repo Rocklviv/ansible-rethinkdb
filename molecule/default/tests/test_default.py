7dfbc2af71db969222650bd8085975d10072cdc3
Denys Chekirda <denys.chekirda@sgdigital.com>
2019-05-19 16:24:55 +0200

    initial commit for unit tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -12,3 +13,38 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+@pytest.mark.parametrize('name, version', [
+    ('rethinkdb', '2.3.6')
+])
+def test_rethinkdb_installed(host, name, version):
+    pkg = host.package(name)
+
+    assert pkg.is_installed
+    assert pkg.version.startswith(version)
+
+def test_check_folders(host):
+    folder = host.file('/var/lib/rethinkdb/default')
+
+    assert folder.exists
+    assert folder.is_directory
+    assert folder.user == "rethinkdb"
+    assert folder.group == "rethinkdb"
+
+
+def test_check_user(host):
+    user = host.user("rethinkdb")
+
+    assert user.name == "rethinkdb"
+    assert user.group == "rethinkdb"
+
+def test_check_group(host):
+    group = host.group("rethinkdb")
+
+    assert group.name == "rethinkdb"
+    assert group.exists
+
+def test_database_is_up(host):
+    assert host.socket("tcp://127.0.0.1:8080").is_listening
+    assert host.socket("tcp://127.0.0.1:28015").is_listening
+    assert host.socket("tcp://127.0.0.1:29015").is_listening
